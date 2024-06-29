551c554d92522f68a8bfb5a402e8aa5c14d2417e
Ryan Hemlock <ryanhemlock@Ryans-Laptop.local>
2024-06-29 11:39:37 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { WeatherComponent } from './weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: 'weather',
+        component: WeatherComponent,
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    }
+];
